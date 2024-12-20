77c177f6c36e4640f1b7751e5ea26e93c4ff95d9
sainath-shetty <86493033+sainath-shetty@users.noreply.github.com>
2024-12-20 23:37:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {User,connectDb}=require('./db');
 connectDb();
 const cors=require("cors");
 const app=express();
-
+app.options('*', cors()); // Enable preflight requests for all routes
 app.use(cors({ 
     origin: "https://cash-ex-frontend.vercel.app", // Replace with your frontend URL
 methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
@@ -13,7 +13,7 @@ credentials: true,
      allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 app.use(express.json());
-app.options('*', cors()); // Enable preflight requests for all routes
+
 
 app.use("/api",rootRouter);
 app.get("/test", (req, res) => {
